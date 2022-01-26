4a758e0d7dd02727840f35b3a1af51daf78012ab
yannickLonjarret <86044285+yannickLonjarret@users.noreply.github.com>
2022-01-26 15:08:56 +0100

    Bouton et menu
    Début de l'interaction aves les boutons et fin du design du jeu

--- app/src/main/java/fr/ldupuis/mentalcounting/MainActivity.java
+++ app/src/main/java/fr/ldupuis/mentalcounting/MainActivity.java
@@ -3,6 +3,9 @@ package fr.ldupuis.mentalcounting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.play_button);
+        Button scoreButton = findViewById(R.id.score_button);
+        
+        playButton.setOnClickListener(view -> openMentalCounting());
+        scoreButton.setOnClickListener(view-> openScore());
+    }
+
+    private void openScore() {
+    }
+
+    private void openMentalCounting() {
     }
 }
--- app/src/main/java/fr/ldupuis/mentalcounting/MentalCounting.java
+++ app/src/main/java/fr/ldupuis/mentalcounting/MentalCounting.java
@@ -3,6 +3,8 @@ package fr.ldupuis.mentalcounting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MentalCounting extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MentalCounting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mental_counting);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.score_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
