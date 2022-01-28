cb2ddd54a5b34834f7ed59b40913020e3a710caf
yannickLonjarret <86044285+yannickLonjarret@users.noreply.github.com>
2022-01-28 11:46:51 +0100

    Menu et page score
    Finition du menu et créations de la page de score

--- app/src/main/java/fr/ldupuis/mentalcounting/MainActivity.java
+++ app/src/main/java/fr/ldupuis/mentalcounting/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         scoreButton.setOnClickListener(view-> openScore());
     }
 
+
     private void openScore() {
     }
 
---
+++ app/src/main/java/fr/ldupuis/mentalcounting/Score.java
@@ -0,0 +1,10 @@
+package fr.ldupuis.mentalcounting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
+
+public class Score extends AppCompatActivity {
+}
