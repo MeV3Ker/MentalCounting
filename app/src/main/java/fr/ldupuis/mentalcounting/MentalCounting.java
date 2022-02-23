47decfa644e9178fca2b475087df29dadfd40a79
MeV3Ker <LD80450@gmail.com>
2022-02-23 13:47:32 +0100

    Submit button code set, need to generate operations

--- app/src/main/java/fr/ldupuis/mentalcounting/MentalCounting.java
+++ app/src/main/java/fr/ldupuis/mentalcounting/MentalCounting.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MentalCounting extends AppCompatActivity {
 
@@ -37,7 +40,6 @@ public class MentalCounting extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -52,5 +54,27 @@ public class MentalCounting extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void Submit() {
+        //submit button
+        Button submitButton = findViewById(R.id.submit_button);
 
+        //in.correct tags
+        TextView correct_D = findViewById(R.id.display_correct_answer);
+        TextView incorrect_D = findViewById(R.id.display_incorrect_answer);
+
+        //operation generator
+        
+
+        //user input
+        EditText input = findViewById(R.id.submit_Operation_answer);
+        String answer = input.getText().toString();
+
+        //init button listener
+        submitButton.setOnClickListener(view -> {
+            if (answer == operation.result)
+                incorrect_D.setVisibility(TextView.VISIBLE);
+            else
+                correct_D.setVisibility(TextView.VISIBLE);
+        });
+    }
 }
